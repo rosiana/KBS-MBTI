951566ab339fd77948a087ca0a6d9398cc6fa6d0
rosiana <rosilutfi@gmail.com>
2015-11-28 16:53:04 +0700

    import salah

---
+++ KBS-MBTI/src/main/java/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rosi
+ */
+import org.apache.jena.*;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        OWLModel owlModel = ProtegeOWL.createJenaOWLModel();
+        owlModel.getNamespaceManager().setDefaultNamespace("http://hello.com#");
+        OWLNamedClass worldClass = owlModel.createOWLNamedClass("World");
+        System.out.println("Class URI: " + worldClass.getURI());
+    } 
+}
