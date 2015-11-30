f5d0991f7a67165c63540af4e65a58eb4d0a1004
rosiana <rosilutfi@gmail.com>
2015-11-30 15:03:23 +0700

    ubah ke java biasa

---
+++ KBSMBTI/src/kbsmbti/KBSMBTI.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kbsmbti;
+
+import org.apache.jena.ontology.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.util.FileManager;
+
+
+/**
+ *
+ * @author Rosi
+ * Kelas percobaan
+ */
+public class KBSMBTI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static final String SOURCE_URL = "./src/kbsmbti/MBTIOutput.owl";
+    protected static final String SOURCE_FILE = "./src/kbsmbti/MBTI.owl";
+
+    public static final String NS = SOURCE_URL + "#";
+
+    /***********************************/
+    /* External signature methods      */
+    /***********************************/
+
+    public static void main( String[] args ) {
+        new KBSMBTI().run();
+    }
+
+    public void run() {
+        OntModel m = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM );
+
+        loadModel( m );
+
+        // get an OntClass reference to one of the classes in the model
+        // note: ideally, we would delegate this step to Jena's schemagen tool
+        OntClass attack = m.getOntClass( NS + "Attack" );
+
+        // similarly a reference to the attack duration property,
+        // and again, using schemagen would be better
+        OntProperty attackDuration = m.getDatatypeProperty( NS + "HasDuration" );
+
+        // create an instance of the attack class to represent the current attack
+        Individual attack01 = m.createIndividual( NS + "attack01", attack );
+
+        // add a duration to the attack
+        attack01.addProperty( attackDuration, m.createTypedLiteral( 10 ) );
+
+        // finally, print out the model to show that we have some data
+        m.write( System.out, "Turtle" );
+    }
+
+    /***********************************/
+    /* Internal implementation methods */
+    /***********************************/
+
+    /** read the ontology and add it as a sub-model of the given ontmodel */
+    protected void loadModel( OntModel m ) {
+        FileManager.get().getLocationMapper().addAltEntry( SOURCE_URL, SOURCE_FILE );
+        Model baseOntology = FileManager.get().loadModel( SOURCE_URL );
+        m.addSubModel( baseOntology );
+
+        // for compactness, add a prefix declaration st: (for Sam Thomas)
+        m.setNsPrefix( "st", NS );
+    }
+    
+}
